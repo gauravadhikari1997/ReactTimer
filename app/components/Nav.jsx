3687770dbbbfaea8334210e30abcc12ef9981d22
Gaurav Adhikari <avjadhikari@gmail.com>
2018-07-01 00:24:52 +0530

    initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function(){
+    return (
+        <div className="top-bar">
+        <div className="float-left">
+          <ul className="dropdown menu" data-dropdown-menu>
+            <li className="menu-text">React Timer App</li>
+            <li><IndexLink to='/timer' activeClassName='active-link'>Timer</IndexLink></li>
+            <li><Link to='/countdown' activeClassName='active-link'>Countdown</Link></li>
+          </ul>
+        </div>
+        <div className="float-right">
+          <ul className="menu">
+            <li><a href="https://gauravadhikari"> Developer</a></li>
+          </ul>
+        </div>
+        </div>
+    );
+  }
+});
+
+module.exports = Nav;
