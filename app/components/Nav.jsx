e060f04a72d3dcd19e40199b609d3ee1c2234081
Gaurav Adhikari <avjadhikari@gmail.com>
2018-07-22 13:08:35 +0530

    edited nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,16 +5,13 @@ var Nav = React.createClass({
   render: function(){
     return (
       <div className="top-bar" id="responsive-menu">
-        <div className="top-bar-left">
         <ul className="menu">
           <li className="menu-text">React App</li>
           <li><IndexLink to='/' activeClassName='active-link'>Timer</IndexLink></li>
           <li><IndexLink to='/countdown' activeClassName='active-link'>Countdown</IndexLink></li>
-        </ul>
-        </div>
-          <div className="top-bar-right">
-            <a href="https://gauravadhikari.com"> Developer</a>
-          </div>
+          <li><a href="https://gauravadhikari.com"> Developer</a></li>
+          </ul>
+
       </div>
     );
   }
